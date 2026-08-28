34c03566530fbadc1e73cae80867cf66923c67cf
ArthurTorres1 <arthurtorres059@gmail.com>
2026-08-27 21:35:48 -0300

    feat: estrutura condicional `if`

--- app.py
+++ app.py
@@ -15,3 +15,12 @@ print('4-Sair\n')
 
 opcao_escolha = int(input('Escolha uma opção: ' ))
 print(f'Você escolheu a opçao {opcao_escolha}\n')
+
+if opcao_escolha == 1:
+    print('Cadastrar Restaurante')
+elif opcao_escolha == 2:
+    print('Listar Restaurantes')
+elif opcao_escolha == 3:
+    print('Ativar Restaurante')
+else:
+    print('Saindo do programa...')
