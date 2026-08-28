725283d6b321c22c06296d3fbe6154b66486a469
ArthurTorres1 <arthurtorres059@gmail.com>
2026-08-28 09:12:43 -0300

    feat: criando funcoes

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-print("""
+import os
+
+def exibir_nome_do_programa():
+    print("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -8,19 +11,35 @@ print("""
 
 """)
 
-print('1-Cadastrar Restaurante')
-print('2-Listar Restaurantes')
-print('3-Ativar Restaurante')
-print('4-Sair\n')
+def exibir_opcoes():
+    print('1-Cadastrar Restaurante')
+    print('2-Listar Restaurantes')
+    print('3-Ativar Restaurante')
+    print('4-Sair\n')
 
-opcao_escolha = int(input('Escolha uma opção: ' ))
-print(f'Você escolheu a opçao {opcao_escolha}\n')
+def finalizar_app():
+    os.system('cls')
+    print('Finalizando o app...\n')
+    exit()
 
-if opcao_escolha == 1:
-    print('Cadastrar Restaurante')
-elif opcao_escolha == 2:
-    print('Listar Restaurantes')
-elif opcao_escolha == 3:
-    print('Ativar Restaurante')
-else:
-    print('Saindo do programa...')
+def escolher_opcao():
+    opcao_escolha = int(input('Escolha uma opção: ' ))
+    print(f'Você escolheu a opçao {opcao_escolha}\n')
+
+    if opcao_escolha == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolha == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolha == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
